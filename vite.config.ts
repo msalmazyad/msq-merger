bcc4fa6abb472429c802868f8f70cd36caaa1a4c
msalmazyad <msalmazyad@iau.edu.sa>
2026-05-23 00:53:29 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// For GitHub Pages: set VITE_BASE to your repo name when deploying.
-// E.g. if the repo is github.com/you/msq-merger, set base to "/msq-merger/".
-const base = process.env.VITE_BASE || "/";
-
 export default defineConfig({
-  base,
+  base: "/msq-merger/",
+
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
